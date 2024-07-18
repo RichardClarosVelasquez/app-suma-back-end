0aeacee868967548e993cdee91974262e722c071
RichardClarosVelasquez <richard.claros@gmail.com>
2024-07-17 23:12:59 -0400

    hablitación de CORS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,10 +9,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors(); // Habilitar CORS
-//   await app.listen(3000);
-// }
-// bootstrap();
